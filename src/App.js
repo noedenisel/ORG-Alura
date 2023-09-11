061bd7183a638055fc3916c96b768d097218f78d
NoeDenise <noelialombardo@hotmail.com>
2023-09-11 16:37:49 -0300

    component header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
+import Header from './components/header/header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/components/header/header.js
@@ -0,0 +1,6 @@
+function Header (){
+return   <img src="/img/header.png" alt='img header org'/>
+}
+
+
+export default Header
