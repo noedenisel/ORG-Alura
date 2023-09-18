691e93d7c546abc896b20d0997e92b89ae76cd8b
NoeDenise <noelialombardo@hotmail.com>
2023-09-18 20:14:54 -0300

    use state

--- src/components/MiOrg/miOrg.js
+++ src/components/MiOrg/miOrg.js
@@ -1,10 +1,25 @@
+import { useState } from "react";
+
 import "./miOrg.css"
 
+
 const MiOrg = () => {
+
+    //console.log(useState());
+
+    const [mostrar, actualizarMostrar] = useState(true)
+    const [nombre, actualizarNombre] = useState()
+
+    const manejarClick = () => {
+        console.log("mostrar/ocultar elemento", !mostrar);
+        actualizarMostrar(!mostrar)
+    }
+
+
     return <section className="orgSection">
-        <h3 className="title"> Mi Organizacion</h3>
-        <img src="/img/add.png" alt="img Add"></img>
-    </section>
+                <h3 className="title"> Mi Organizacion {nombre}</h3>
+                <img src="/img/add.png" alt="img Add" onClick={manejarClick}></img>
+            </section>
 }
 
 export default MiOrg
